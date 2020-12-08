0bc5c4b0ef75a24546603c73cbbf7dacc0435d68
andradazoltan <andrada.12321@gmail.com>
2020-12-08 01:41:59 -0800

    Host side coded and builds, not yet tested

--- snappy/pim-snappy/pim_snappy.c
+++ snappy/pim-snappy/pim_snappy.c
@@ -6,6 +6,7 @@
 #include <limits.h>
 #include <getopt.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 #include <dpu.h>
 #include <dpu_memory.h>
@@ -14,37 +15,49 @@
 #include "pim_snappy.h"
 #include "PIM-common/common/include/common.h"
 
+#define REQUESTS_TO_WAIT_FOR NR_TASKLETS
+#define MAX_TIME_WAIT 1
+#define MAX_INPUT_SIZE (256 * 1024)
+#define MAX_OUTPUT_SIZE (512 * 1024)
+
 // Buffer context struct for input and output buffers on host
 typedef struct host_buffer_context
 {
-    uint8_t *buffer;        // Entire buffer
-    uint8_t *curr;          // Pointer to current location in buffer
+    char *buffer;        // Entire buffer
+    char *curr;          // Pointer to current location in buffer
     uint32_t length;        // Length of buffer
 } host_buffer_context_t;
 
-// Arguments passed to a particular thread
-struct thread_args {
+// Arguments passed by a particular thread
+typedef struct caller_args {
 	int data_ready;
-	host_buffer_context_t **input;  // Input buffer
-	host_buffer_context_t **output; // Output buffer
+	host_buffer_context_t *input;  // Input buffer
+	host_buffer_context_t *output; // Output buffer
 	int retval;
-};
+} caller_args_t;
+
+typedef struct master_args {
+	int stop_thread;		// Set to 1 to end dpu_master_thread
+	uint32_t req_head;			// Next slot availble in caller_args
+	uint32_t req_tail;			// Next slot to be loaded to DPU in caller_args
+	uint32_t req_tail_dispatched;// 
+	uint32_t req_count;			// Number of occupied slots in caller_args
+	caller_args_t **caller_args;
+} master_args_t;
 
 // Stores allocated DPUs
 static struct dpu_set_t dpus;
-static num_ranks = 0;
-static num_dpus = 0;
+static uint32_t num_ranks = 0;
+static uint32_t num_dpus = 0;
 
 // Thread variables
-static pthread_mutex_t host_mutex;
-static pthead_cond_t host_cond;
-
-static pthread_mutex_t dpu_mutex;
+static pthread_mutex_t mutex;
+static pthread_cond_t caller_cond;
 static pthread_cond_t dpu_cond;
-static pthread_t *threads;
 
-static struct thread_args *thread_args;
-static int *slots_remaining_per_thread;
+static pthread_t dpu_master_thread;
+static master_args_t args;
+static uint32_t total_request_slots = 0;
 
 /**
  * Attempt to read a varint from the input buffer. The format of a varint
@@ -74,83 +87,168 @@ static inline bool read_varint32(struct host_buffer_context *input, uint32_t *va
 }
 
 /**
- * Perform the Snappy decompression on the DPU.
+ * Get a bitmap of the free ranks currently available.
  *
- * @param input: holds input buffer information
- * @param output: holds output buffer information
- * @return 1 if successful, 0 otherwise
+ * @param free_ranks: each bit is set to 1 if that rank is
+ *                    currently available
+ */
+static void get_free_ranks(uint32_t* free_ranks) {
+    struct dpu_set_t dpu_rank;
+	uint32_t rank_id = 0;
+	
+	DPU_RANK_FOREACH(dpus, dpu_rank) {
+		bool done = 0, fault = 0;
+
+		// Check if any rank is free
+		dpu_status(dpu_rank, &done, &fault);
+		if (fault) {
+			fprintf(stderr, "Fault on DPU rank %d\n", rank_id);
+			// TODO: error handle
+		}
+
+		if (done) 
+			*free_ranks |= (1 << rank_id);
+
+		rank_id++;
+	}
+}
+
+static void load_rank(struct dpu_set_t *dpu_rank, master_args_t *args) {
+	uint32_t idx = args->req_tail_dispatched;
+	uint32_t start_idx = args->req_tail_dispatched;
+
+	struct dpu_set_t dpu;
+	for (int i = 0; i < NR_TASKLETS; i++) {
+		// Copy the index of the request, input and output lengths
+		uint32_t max_input_length = 0;
+		DPU_FOREACH(*dpu_rank, dpu) {
+			if (idx == args->req_head)
+				break;
+ 
+			DPU_ASSERT(dpu_copy_to(dpu, "req_idx", i * sizeof(uint32_t), &idx, sizeof(uint32_t)));
+			DPU_ASSERT(dpu_copy_to(dpu, "input_length", i * sizeof(uint32_t), &(args->caller_args[idx]->input->length), sizeof(uint32_t)));
+			DPU_ASSERT(dpu_copy_to(dpu, "output_length", i * sizeof(uint32_t), &(args->caller_args[idx]->output->length), sizeof(uint32_t))); 
+
+			// Update max input length
+			max_input_length = MAX(max_input_length, args->caller_args[idx]->input->length);
+
+			idx = (idx + 1) % total_request_slots;
+		}
+
+		// Copy the input buffer 
+		idx = start_idx;
+		DPU_FOREACH(*dpu_rank, dpu) {
+			if (idx == args->req_head)
+				break;
+
+			DPU_ASSERT(dpu_prepare_xfer(dpu, (void *)args->caller_args[idx]->input->curr));
+			idx = (idx + 1) % total_request_slots;
+		}
+		DPU_ASSERT(dpu_push_xfer(*dpu_rank, DPU_XFER_TO_DPU, "input_buffer", i * MAX_INPUT_SIZE, ALIGN(max_input_length, 8), DPU_XFER_DEFAULT));
+
+		start_idx = idx;
+	}
+
+	args->req_tail_dispatched = idx;
+	
+	// Launch the rank
+	dpu_launch(*dpu_rank, DPU_ASYNCHRONOUS);
+}
+
+static void unload_rank(struct dpu_set_t *dpu_rank, master_args_t *args) {
+	struct dpu_set_t dpu;
+	for (int i = 0; i < NR_TASKLETS; i++) {
+		uint32_t max_output_length = 0;
+		DPU_FOREACH(*dpu_rank, dpu) {
+			// Get the request index
+			uint32_t req_idx = 0;
+			DPU_ASSERT(dpu_copy_from(dpu, "req_idx", i * sizeof(uint32_t), &req_idx, sizeof(uint32_t)));
+			// TODO fix this in case a batch is skipped
+			if (req_idx == args->req_tail) {
+				args->req_count--;
+				args->req_tail = req_idx;
+			}
+
+			// Get the output length
+			uint32_t output_length = 0;
+			DPU_ASSERT(dpu_copy_from(dpu, "output_length", i * sizeof(uint32_t), &output_length, sizeof(uint32_t)));
+			max_output_length = MAX(max_output_length, output_length);
+
+			// Get the return value
+			DPU_ASSERT(dpu_copy_from(dpu, "retval", i * sizeof(uint32_t), &(args->caller_args[req_idx]->retval), sizeof(uint32_t)));
+			args->caller_args[req_idx]->data_ready = 1;
+
+			// Set up the transfer
+			DPU_ASSERT(dpu_prepare_xfer(dpu, (void *)args->caller_args[req_idx]->output->curr));
+		}
+
+		DPU_ASSERT(dpu_push_xfer(*dpu_rank, DPU_XFER_FROM_DPU, "output_buffer", i * MAX_OUTPUT_SIZE, ALIGN(max_output_length, 8), DPU_XFER_DEFAULT));
+	}
+}
+
+double timediff(struct timeval *start, struct timeval *end) {
+	double start_time = start->tv_sec + start->tv_usec / 1000000.0;
+	double end_time = end->tv_sec + end->tv_usec / 1000000.0;
+	return abs(end_time - start_time);
+}
+
+/**
+ * Perform the Snappy decompression on the DPU.
  */
 static void * dpu_uncompress(void *arg) {
 	// Get the thread arguments
-	struct thread_args *args = (struct thread_args *)arg;
+	master_args_t *args = (master_args_t *)arg;
 
-	while (args->data_ready != -1) { // -1 means the thread is being terminated
-		// Wait until data is ready 
-		pthread_mutex_lock(&host_mutex);
-		while (args->data_ready != 1) {
-			pthread_cond_wait(&dpu_cond, &host_mutex);
+	struct timeval first, second;
+	uint32_t ranks_dispatched = 0;
+	while (args->stop_thread != 1) { 
+		// Wait until there are enough queued requests
+		pthread_mutex_lock(&mutex);
+		gettimeofday(&first, NULL);
+		while ((args->req_count < REQUESTS_TO_WAIT_FOR) || (timediff(&first, &second) > MAX_TIME_WAIT)) {
+			pthread_cond_wait(&dpu_cond, &mutex);
+			gettimeofday(&second, NULL);
+		}
+		pthread_mutex_unlock(&mutex);
+
+		// Get the list of ranks currently free
+		uint32_t free_ranks = 0;
+		get_free_ranks(&free_ranks);
+
+		// Dispatch all the requests we currently have
+		uint32_t rank_id = 0;	
+    	struct dpu_set_t dpu_rank;
+		DPU_RANK_FOREACH(dpus, dpu_rank) {
+			if (free_ranks & (1 << rank_id)) {
+				pthread_mutex_lock(&mutex);
+				load_rank(&dpu_rank, args); 
+				pthread_mutex_unlock(&mutex);
+
+				ranks_dispatched |= (1 << rank_id);
+			}
+			rank_id++;
+		}
+
+		// If any previously dispatched requests are done, read back the data
+		rank_id = 0;
+		DPU_RANK_FOREACH(dpus, dpu_rank) {
+			if (ranks_dispatched & (1 << rank_id)) {
+				if (free_ranks & (1 << rank_id)) {
+					pthread_mutex_lock(&mutex);
+					unload_rank(&dpu_rank, args);
+					pthread_mutex_unlock(&mutex);
 			
+					ranks_dispatched &= ~(1 << rank_id);
 
-    struct dpu_set_t dpu_rank;
-    struct dpu_set_t dpu;
+					// Signal that data is ready
+					pthread_cond_broadcast(&caller_cond);
+				}
+			}
+			rank_id++;
+		}
+	}	
 
-    // Calculate input length of only the decompressed stream
-	uint32_t input_offset[NR_DPUS][NR_TASKLETS] = {0};
-	uint32_t output_offset[NR_DPUS][NR_TASKLETS] = {0};
-
-    uint32_t dpu_idx = 0;
-    DPU_RANK_FOREACH(dpus, dpu_rank) {
-        uint32_t starting_dpu_idx = dpu_idx;
-        DPU_FOREACH(dpu_rank, dpu) {
-            // Check to get rid of array bounds compiler warning
-            if (dpu_idx >= NR_DPUS)
-                break;
-
-			uint32_t input_length = input->length - (input->curr - input->buffer);
-            DPU_ASSERT(dpu_copy_to(dpu, "input_length", 0, &(input_length), sizeof(uint32_t)));
-            DPU_ASSERT(dpu_copy_to(dpu, "output_length", 0, &(output->length), sizeof(uint32_t)));
-
-            DPU_ASSERT(dpu_prepare_xfer(dpu, (void *)(input->curr + input_offset[dpu_idx][0])));
-            dpu_idx++;
-        }
-        DPU_ASSERT(dpu_push_xfer(dpu_rank, DPU_XFER_TO_DPU, "input_buffer", 0, ALIGN(input->length, 8), DPU_XFER_DEFAULT));
-
-        dpu_idx = starting_dpu_idx;
-        DPU_FOREACH(dpu_rank, dpu) {
-            DPU_ASSERT(dpu_prepare_xfer(dpu, (void *)input_offset[dpu_idx]));
-            dpu_idx++;
-        }
-        DPU_ASSERT(dpu_push_xfer(dpu_rank, DPU_XFER_TO_DPU, "input_offset", 0, sizeof(uint32_t) * NR_TASKLETS, DPU_XFER_DEFAULT));
-
-        dpu_idx = starting_dpu_idx;
-        DPU_FOREACH(dpu_rank, dpu) {
-            DPU_ASSERT(dpu_prepare_xfer(dpu, (void *)output_offset[dpu_idx]));
-            dpu_idx++;
-        }
-        DPU_ASSERT(dpu_push_xfer(dpu_rank, DPU_XFER_TO_DPU, "output_offset", 0, sizeof(uint32_t) * NR_TASKLETS, DPU_XFER_DEFAULT));
-    }
-
-    // Launch all DPUs
-    int ret = dpu_launch(dpus, DPU_SYNCHRONOUS);
-    if (ret != 0)
-    {
-        DPU_ASSERT(dpu_free(dpus));
-        return false;
-    }
-
-    // Deallocate the DPUs
-    dpu_idx = 0;
-    DPU_RANK_FOREACH(dpus, dpu_rank) {
-        DPU_FOREACH(dpu_rank, dpu) {
-            // Get the results back from the DPU
-            DPU_ASSERT(dpu_prepare_xfer(dpu, (void *)(output->buffer + output_offset[dpu_idx][0])));
-            dpu_idx++;
-        }
-
-        DPU_ASSERT(dpu_push_xfer(dpu_rank, DPU_XFER_FROM_DPU, "output_buffer", 0, ALIGN(output->length, 8), DPU_XFER_DEFAULT));
-    }
-
-    return true;
+	return NULL;
 }
 
 int pim_init(void) {
@@ -159,46 +257,36 @@ int pim_init(void) {
 	
 	dpu_get_nr_ranks(dpus, &num_ranks);
 	dpu_get_nr_dpus(dpus, &num_dpus);
+	total_request_slots = num_dpus * NR_TASKLETS;
 
 	// Load the program to all DPUs
     DPU_ASSERT(dpu_load(dpus, DPU_PROGRAM, NULL));
 
-	// Create a thread per rank
-	threads = (pthread_t *)malloc(sizeof(pthread_t) * num_ranks);
-	thread_args = (struct thread_args *)malloc(sizeof(struct thread_args) * num_ranks);
-	for (int i = 0; i < num_ranks; i++) {
-		struct thread_args *args = thread_args[i];
-		args->data_ready = 0;
-		args->input = &((host_buffer_context_t *)malloc(sizeof(host_buffer_context_t) * NR_TASKLETS * (num_dpus / num_ranks)));	
-		args->output = &((host_buffer_context_t *)malloc(sizeof(host_buffer_context_t) * NR_TASKLETS * (num_dpus / num_ranks)));	
-
-		if (pthread_create(&threads[i], NULL, dpu_decompress, args) != 0) {
-			fprintf(stderr, "Failed to create dpu_decompress pthreads\n");
-			return -1;
-		}
-	}
-
-	// Initialize number of data slots remaining per thread
-	slots_remaining_per_thread = (int *)malloc(sizeof(int) * num_ranks);
-	memset(slots_remaining_per_thread, NR_TASKLETS * (num_dpus / num_ranks), sizeof(int) * num_ranks);
-
-	// Create mutex to guard access to DPUs
-	if (pthread_mutex_init(&host_mutex, NULL) != 0) {
-		fprintf(stderr, "Failed to create host mutex\n");
-		return -1;
-	}
-	if (pthread_mutex_init(&dpu_mutex, NULL) != 0) {
-		fprintf(stderr, "Failed to create dpu mutex\n");
+	// Create the DPU master host thread
+	args.stop_thread = 0;
+	args.req_head = 0;
+	args.req_tail = 0;
+	args.req_count = 0;
+	args.caller_args = (caller_args_t **)malloc(sizeof(caller_args_t *) * total_request_slots);
+	
+	if (pthread_create(&dpu_master_thread, NULL, dpu_uncompress, &args) != 0) {
+		fprintf(stderr, "Failed to create dpu_decompress pthreads\n");
 		return -1;
 	}
 
-	// Create condition variable to wake up threads when data is ready
-	if (pthread_cond_init(&host_cond, NULL) != 0) {
-		fprintf(stderr, "Faled to create condition variable\n");
+	// Create mutex and condition variable for calling threads
+	if (pthread_mutex_init(&mutex, NULL) != 0) {
+		fprintf(stderr, "Failed to create mutex\n");
+		return -1;
+	}
+
+	// Create condition variables for both directions
+	if (pthread_cond_init(&caller_cond, NULL) != 0) {
+		fprintf(stderr, "Faled to create calller condition variable\n");
 		return -1;
 	}
 	if (pthread_cond_init(&dpu_cond, NULL) != 0) {
-		fprintf(stderr, "Faled to create condition variable\n");
+		fprintf(stderr, "Faled to create dpu condition variable\n");
 		return -1;
 	}
 
@@ -210,73 +298,69 @@ void pim_deinit(void) {
 	num_ranks = 0;
 	num_dpus = 0;
 
-	// Signal to terminate the threads
-	for (int i = 0; i < num_ranks; i++) {
-		thread_args[i]->data_ready = -1;
-	}
-	pthread_cond_broadcast(&cond);
+	// Signal to terminate the dpu master thread
+	pthread_mutex_lock(&mutex);
+	args.stop_thread = 1;
+	pthread_mutex_unlock(&mutex);
+	pthread_cond_broadcast(&dpu_cond);
+
+	pthread_join(dpu_master_thread, NULL);
 
 	// Free all the allocated memory
-	for (int i = 0; i < num_ranks; i++) {
-		pthread_join(threads[i], NULL);
-
-		struct thread_args *args = thread_args[i];
-		free(*(args->input));
-		free(*(args->output));
-		free(args);
-	}	
-	free(threads);
-	free(thread_args);
-	free(slots_remaining_per_thread);
+	free(args.caller_args);
 
 	// Destroy the mutex
-	pthread_mutex_destroy(&host_mutex);
-	pthread_mutex_destroy(&dpu_mutex);
+	pthread_mutex_destroy(&mutex);
 
-	// Destroy the condition variable
-	pthread_cond_destroy(&host_cond);
+	// Destroy the condition variables
+	pthread_cond_destroy(&caller_cond);
 	pthread_cond_destroy(&dpu_cond);
 }
 
 int pim_decompress(const char *compressed, size_t compressed_length, char *uncompressed) {
-	for (int i = 0; i < num_ranks; i++) {
-		pthread_mutex_lock(&host_mutex);
-		
-		slot = slots_remaining_per_thread[i];
-		if (slot != 0) {
-			struct thread_args *args = thread_args[i];	
+	// Set up in the input and output buffers
+	host_buffer_context_t input = {
+		.buffer = (char *)compressed,
+		.curr   = (char *)compressed,
+		.length = compressed_length
+	};
 
-			args->input[slot - 1]->buffer = compressed;
-			args->input[slot - 1]->curr = compressed;
-			args->input[slot - 1]->length = compressed_length;
+	host_buffer_context_t output = {
+		.buffer = uncompressed,
+		.curr   = uncompressed,
+		.length = 0
+	};
 
-			args->output[slot - 1]->buffer = uncompressed;
-			args->output[slot - 1]->curr = uncompressed;
-			args->output[slot - 1]->length = 0;
-
-			// Read the decompressed length
-			if (!read_varint32(args->input[slot - 1], &(args->output[slot - 1]->length))) {
-				fprintf(stderr, "Failed to read decompressed length\n");
-				return false;
-			}
-
-			slots_remaining_per_thread[i]--;
-			if (slots_remaining_per_thread[i] == 0) {
-				args->data_ready = 1;
-				pthread_cond_broadcast(&dpu_cond);
-			}
-
-			// Wait for the data to be returned
-			while (args->data_ready != 0) {
-				pthread_cond_wait(&host_cond, &host_mutex);
-			}
-			pthread_mutex_unlock(&host_mutex);
-
-			return args->retval;
-		}
-
-		pthread_mutex_unlock(&host_mutex);
+	// Read the decompressed length
+	if (!read_varint32(&input, &(output.length))) {
+		fprintf(stderr, "Failed to read decompressed length\n");
+		return false;
 	}
 
-	return false;
+	// Set up the caller arguments
+	caller_args_t m_args = {
+		.data_ready = 1,
+		.input = &input,
+		.output = &output,
+		.retval = 0
+	};	
+	
+	pthread_mutex_lock(&mutex);
+
+	// Wait until there is space to take in more requests
+	while (args.req_count == total_request_slots) {
+		pthread_cond_wait(&caller_cond, &mutex);
+	}
+	
+	args.caller_args[args.req_head] = &m_args;
+	args.req_head = (args.req_head + 1) % total_request_slots;
+    args.req_count++;
+	pthread_cond_broadcast(&dpu_cond);
+
+	while (m_args.data_ready != 0) {
+		pthread_cond_wait(&caller_cond, &mutex);
+	}
+	pthread_mutex_unlock(&mutex);
+
+	return m_args.retval;
 }
